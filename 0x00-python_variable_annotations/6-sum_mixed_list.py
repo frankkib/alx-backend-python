61f47fa11b949e16968afdd610e513460ed3a9d2
frankkib <frankkibagendi13@gmail.com>
2023-08-03 10:45:50 +0000

    function that takes a mixed list and returns a float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@ from typing import List, Union
 """function that takes mixed list and returns a float"""
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """returns float"""
-    return sum(mxd_list)
+    return sum(mxd_lst)
