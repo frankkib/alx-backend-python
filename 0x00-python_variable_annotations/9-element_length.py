2ac6be99fd931a6e626ca1bd9de822f197c4402d
frankkib <frankkibagendi13@gmail.com>
2023-08-04 06:51:41 +0000

    fixed the values annotated

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """function that takes alist  and return a list"""
-from typing import Iterable, Sequence, Tuple
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Iterable[str]) -> Sequence[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """return list"""
     return [(i, len(i)) for i in list]
