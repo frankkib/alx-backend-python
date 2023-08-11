0629926957a3e75ee51f1a08422de4056691cb55
frankkib <frankkibagendi13@gmail.com>
2023-08-11 07:34:40 +0000

    fixed the list typing error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Asyncgronous function comprehention"""
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """returns asynchronous generator"""
     return [i async for i in async_generator()]
