9c55853991a99f2f24dc6686f672ba051f2b0b8b
frankkib <frankkibagendi13@gmail.com>
2023-08-11 07:30:52 +0000

    Added a generator type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() ->Generator[float , None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """returns list of random floats"""
     for _ in range(10):
         await asyncio.sleep(1)
