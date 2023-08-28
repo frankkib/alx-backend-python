906ef868aea93ffe6c311187f9ab04fc738dad05
frankkib <frankkibagendi13@gmail.com>
2023-08-28 10:00:26 +0000

    Use the assertRaises context manager to test that a KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 
 
-class TestAcessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """The test class for access nested map function"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -16,6 +16,16 @@ class TestAcessNestedMap(unittest.TestCase):
         """Access function"""
         nest = access_nested_map(nested_map, path)
         self.assertEqual(nest, result)
+
+    @parameterized.expand([
+        ({}, ("a",), ("a")),
+        ({"a": 1}, ("a", "b"), "b"),
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, key):
+        """Exception function"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), f"'{key}'")
 
 
 if __name__ == "__main__":
